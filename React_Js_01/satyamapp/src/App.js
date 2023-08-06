dbec2e9137ba6c16900611649265d797befbe45b
satyam <ss8454015344@gmail.com>
2023-08-07 00:39:23 +0530

    first commit

---
+++ React_Js_01/satyamapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Resturant from './components/Basics/Resturant.js';
+// import UseEffect from './components/Hooks/useEffect';
+// import UseReducer from './components/Hooks/useReducer';
+import UseState from './components/Hooks/useState';
+
+const App = () => {
+  return <UseState/>;
+}
+
+export default App
